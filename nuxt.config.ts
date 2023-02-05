ca170367b3c8567f042e567327d798741cd1fe13
Dominik Havel <dmnkhvl@gmail.com>
2023-02-05 17:02:46 +0100

    feat: add Vercel Analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
+  plugins: [{ src: "~/plugins/vercel.ts", mode: "client" }],
   modules: [
     "@nuxtjs/tailwindcss",
     "@nuxtjs/i18n",
