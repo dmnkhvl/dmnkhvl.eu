b34411ee749f35a3df25854bd86e8a3f51b9cc96
Dominik Havel <dmnkhvl@gmail.com>
2023-02-06 18:21:02 +0100

    feat: add CV in Slovak

--- components/DownloadCV.vue
+++ components/DownloadCV.vue
@@ -1,6 +1,15 @@
 <template>
   <a
+    v-if="$i18n.locale === 'en'"
     href="/dominikHavel_en.pdf"
+    target="_blank"
+    class="text-special decoration-special/20 hover:decoration-special mt-2 flex items-center gap-x-1 underline decoration-dotted underline-offset-4 transition duration-300 ease-in-out hover:decoration-solid"
+  >
+    <nuxt-icon name="resume" />{{ $t("viewResume") }}</a
+  >
+  <a
+    v-else
+    href="/dominikHavel_sk.pdf"
     target="_blank"
     class="text-special decoration-special/20 hover:decoration-special mt-2 flex items-center gap-x-1 underline decoration-dotted underline-offset-4 transition duration-300 ease-in-out hover:decoration-solid"
   >
