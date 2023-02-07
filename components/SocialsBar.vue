ac7785608e82d0c64c3e15f8044a482fcb49d5f1
Dominik Havel <dmnkhvl@gmail.com>
2023-02-07 13:10:12 +0100

    style: add hover socials bar

--- components/SocialsBar.vue
+++ components/SocialsBar.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="mt-1 items-center gap-x-4">
-    <ExternalLink href="https://github.com/dmnkhvl"
-      ><nuxt-icon name="github" class="text-xl text-white"
+    <ExternalLink href="https://github.com/dmnkhvl "
+      ><nuxt-icon name="github" class="hover:text-special text-xl text-white"
     /></ExternalLink>
     <ExternalLink href="https://www.linkedin.com/in/dmnkhvl/"
-      ><nuxt-icon name="linkedin" class="text-xl text-white"
+      ><nuxt-icon name="linkedin" class="hover:text-special text-xl text-white"
     /></ExternalLink>
     <ExternalLink href="https://twitter.com/dmnkhvl"
-      ><nuxt-icon name="twitter" class="text-xl text-white"
+      ><nuxt-icon name="twitter" class="hover:text-special text-xl text-white"
     /></ExternalLink>
   </div>
 </template>
